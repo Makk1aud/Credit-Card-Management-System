104e5f3723c717840c93c8480fff97fa85305a58
Makk1aud <1roman080283@mail.ru>
2023-05-23 20:23:25 +0600

    Data Base Users connection

--- Pages/PageRegistration.xaml.cs
+++ Pages/PageRegistration.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Card_management_system.DataApp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,21 +27,48 @@ namespace Card_management_system.Pages
             comboBoxGender.ItemsSource = new List<string>() { "Муж", "Жен", "Нет" };
         }
 
-        public bool CountNumbers(TextBox textBox) => textBox.Text.Where(x => Char.IsNumber(x)).Count() == 0;
+        public bool CountNumbers(TextBox textBox)
+        {
+            if (textBox.Text.Where(x => Char.IsNumber(x)).Count() == 0 && !String.IsNullOrEmpty(textBox.Text))
+                return true;
+            textBox.Text = null;
+            return false;
+        }
 
         public bool CountNumbers(PasswordBox passwordBox) => passwordBox.Password.Where(x => Char.IsNumber(x)).Count() > 0;
 
         public bool CheckEmail(TextBox textBox) => textBox.Text.Count(t => t == '@') > 0 && textBox.Text.Count(t => t == '@') < 2;
 
+        private bool CheckPassword(PasswordBox passwordBox) => CountNumbers(passwordBox) && passwordBox.Password.Length > 7;
+
+        private bool CheckUserInputData()
+        {
+            if (CheckPassword(passwordBoxPassFirst) && !Equals(passwordBoxPassFirst.Password, passwordBoxPassSec.Password))
+            {
+                MessageBox.Show("Пароли должный совпадать и иметь хотя бы 1 цифру");
+                return false;
+            }
+            return CountNumbers(textBoxName) && CountNumbers(textBoxSurname)
+                && CheckEmail(textBoxEmail) && textBoxTelephone.Text.Length == 12;    
+        }
+
         private void buttonRegistration_Click(object sender, RoutedEventArgs e)
         {
-            if(CountNumbers(textBoxName) && CountNumbers(textBoxSurname))
+            if (!CheckUserInputData())
+                return;
+            Users users = new Users()
             {
-                MessageBox.Show("Цифр нет");
-            }
-            if (CheckEmail(textBoxEmail))
-                MessageBox.Show("nice");
-            
+                name = textBoxName.Text,
+                surname = textBoxSurname.Text,
+                patronymic = "-",
+                gender = comboBoxGender.Text,
+                number = textBoxTelephone.Text,
+                login = textBoxEmail.Text,
+                password = passwordBoxPassFirst.Password,
+                roleid = 2
+            };
+            PageClass.connectDB.Users.Add(users);
+            PageClass.connectDB.SaveChanges();
         }
 
         private void textBoxTelephone_TextChanged(object sender, TextChangedEventArgs e)
@@ -49,14 +77,5 @@ namespace Card_management_system.Pages
                 textBoxTelephone.Text = "+" + textBoxTelephone.Text;
         }
 
-        private bool CheckPassword(PasswordBox passwordBox) => CountNumbers(passwordBox) && passwordBox.Password.Length > 8;
-
-        private void CheckEqualPassword(object sender, RoutedEventArgs e)
-        {
-            if (CheckPassword(passwordBoxPassFirst) && Equals(passwordBoxPassFirst.Password, passwordBoxPassSec.Password))
-                MessageBox.Show("Equal");
-            else
-                MessageBox.Show("Пароли должный совпадать");
-        }
     }
 }
