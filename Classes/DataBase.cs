91522dbe64c7682bcf0d5363ef8bd0d7b5d7e087
Makk1aud <1roman080283@mail.ru>
2023-06-05 17:25:28 +0600

    Make a DataBase class and refactoring

---
+++ Classes/DataBase.cs
@@ -0,0 +1,30 @@
+﻿using Card_management_system.DataApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Serialization;
+
+namespace Card_management_system.Classes
+{
+    static class DataBase
+    {
+        private static void SaveChangesDataBase(string message)
+        {
+            try
+            {
+                PageClass.connectDB.SaveChanges();
+                MessageBox.Show(message, 
+                    "Объявление", 
+                    MessageBoxButton.OK, 
+                    MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
