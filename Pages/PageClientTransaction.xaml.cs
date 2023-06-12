11ba0406e700196b362dcfc744efd17cc7883f5c
Makk1aud <1roman080283@mail.ru>
2023-06-12 18:39:54 +0600

    Make a sort DataGrid data

--- Pages/PageClientTransaction.xaml.cs
+++ Pages/PageClientTransaction.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Card_management_system.Classes;
+using Card_management_system.DataApp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,11 +25,60 @@ namespace Card_management_system.Pages
         public PageClientTransaction()
         {
             InitializeComponent();
+            dataGridTransactions.ItemsSource = DefaultList();
+
+            comboBoxMoneySort.ItemsSource = new List<string>() { "По возрастанию", "По убыванию" };
+        }
+
+        public List<Transactions> DefaultList() => PageClass.connectDB.Transactions.ToList();
+
+        public void FillingDataGrid()
+        {
+            var listSort = DefaultList();
+            ComboBoxSort(ref listSort);
+            TextBoxSort(ref listSort);
+            DatePickerSort(ref listSort);
+            dataGridTransactions.ItemsSource = listSort;
+        }
+
+        private void ComboBoxSort(ref List<Transactions> listSort)
+        {
+            if (comboBoxMoneySort.SelectedIndex == 0)
+                listSort = listSort.OrderBy(x => x.moneysum).ToList();
+            else if (comboBoxMoneySort.SelectedIndex == 1)
+                listSort = listSort.OrderByDescending(x => x.moneysum).ToList();
+        }
+
+        private void TextBoxSort( ref List<Transactions> listSort)
+        {
+            if(textBoxMoneySort.Text != String.Empty)
+                listSort = listSort.Where(x => x.moneysum > int.Parse(textBoxMoneySort.Text)).ToList();
+        }
+
+        private void DatePickerSort(ref List<Transactions> listSort)
+        {
+            if(datePickerTransactionSort.SelectedDate != null)
+                listSort = listSort.Where(x => x.transactiondate == datePickerTransactionSort.SelectedDate).ToList();
         }
 
         private void buttonBack_Click(object sender, RoutedEventArgs e)
         {
+            PageClass.frameObject.GoBack();
+        }
 
+        private void datePickerTransactionSort_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FillingDataGrid();
+        }
+
+        private void comboBoxMoneySort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FillingDataGrid();
+        }
+
+        private void textBoxMoneySort_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            FillingDataGrid();
         }
     }
 }
